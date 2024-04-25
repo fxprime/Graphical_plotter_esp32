e77ff9ba3547261225683731626b08bafd2f44cc
ArduinoNa ขายอุปกรณ์ Arduino, เครื่องมือช่าง, raspberry pi, สอน ROS <thanabadee_bc@hotmail.com>
2024-04-25 14:59:43 +0700

    Update code

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,25 @@
-#include <Arduino.h>
-
-void setup()
-{
-    Serial.begin(115200); 
+#include <Arduino.h> 
+void setup() {
+  Serial.begin(115200);
+  // Print log
+  Serial.println("setup");
 }
 
-float i = 0;
-void loop()
-{
-    i += 0.1;
+float i=0;
+void loop() {
+  i+=0.1;
 
-    Serial.print(sin(i));
-    Serial.print("\t");
-    Serial.println(cos(i));
-    delay(50);
+  // Print log
+  Serial.print("loop");
+  Serial.println(i);
+  
+  // Plot a sine
+  Serial.print(">sin:");
+  Serial.println(sin(i));
+
+  // Plot a cos
+  Serial.print(">cos:");
+  Serial.println(cos(i));
+    
+  delay(50);
 }
